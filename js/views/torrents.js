4402719eb8a7e74324f322106ee0c092c9da63f0
endor <fp@notjusthosting.com>
2010-01-20 00:11:51 +0200

    used icons instead of buttons for pausing/activation

--- js/views/torrents.js
+++ js/views/torrents.js
@@ -5,10 +5,10 @@ TorrentsView = function(torrents) {
     'torrents': this.torrents,
     'pauseAndActivateButton': function() {
       var torrent = Torrent(this);
-      var method = torrent.isActive() ? ['torrent-stop', 'Pause'] : ['torrent-start', 'Activate'];
+      var method = torrent.isActive() ? ['torrent-stop', 'Pause', 'pause'] : ['torrent-start', 'Activate', 'activate'];
       return '<form action="#/torrents/' + torrent.id + '" method="PUT">' +
              '<input type="hidden" name="method" value="' + method[0] + '"/>' +
-             '<input type="submit" value="' + method[1] + '"/></form>';
+             '<input type="submit" value="' + method[1] + '" class="pauseAndActivateButton ' + method[2] + '" /></form>';
     }    
   };
   
