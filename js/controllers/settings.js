c212c9ddfb7c955aa8726d017c1130b8ef13394f
endor <fp@notjusthosting.com>
2010-02-03 10:10:49 +0200

    show if peer port is open

--- js/controllers/settings.js
+++ js/controllers/settings.js
@@ -16,11 +16,13 @@ Settings = function(sammy) { with(sammy) {
       context.partial('./templates/settings/index.mustache', view, function(rendered_view) {
         context.openInfo(rendered_view);
         trigger('settings-refreshed', view);
+        updatePeerPortDiv();
       });
     });
   });
   
   put('#/settings', function() {
+    var context = this;
     var request = { 'method': 'session-set' };
     if(this.params['alt-speed-enabled']) {
       request['arguments'] = this.turtle_mode_hash(this.params['alt-speed-enabled']);
@@ -29,9 +31,21 @@ Settings = function(sammy) { with(sammy) {
     };
     rpc.query(request, function(response) {
       trigger('flash', 'Settings updated successfully');
+      if(context.params['peer-port']) { updatePeerPortDiv(); }
     });
   });
     
+  function updatePeerPortDiv() {
+    var request = { 'method': 'port-test', 'arguments': {} };
+    rpc.query(request, function(response) {
+      if(response['port-is-open']) {
+        $('#port-open').addClass('active');
+      } else {
+        $('#port-open').removeClass('active');
+      }
+    });
+  };
+  
   bind('settings-refreshed', function(e, settings){ with(this) {
     this.updateSettingsCheckboxes(settings);
     this.menuizeInfo();
--- spec/torrent_helpers_spec.js
+++ spec/torrent_helpers_spec.js
@@ -1,26 +1,26 @@
 describe 'TorrentHelpers'
   before_each
-    $('body').append(elements(fixture('torrents')).get(0));
-    old_torrents = $('.torrent');
-    torrent_helpers = TorrentHelpers;
+    $('body').append(elements(fixture('torrents')).get(0))
+    old_torrents = $('.torrent')
+    torrent_helpers = TorrentHelpers
   end
 
   after_each
-    $('body').find('#torrents').remove();
+    $('body').find('#torrents').remove()
   end
 
   describe 'removeOldTorrents'    
     it 'should not remove torrents that came in with the update'
-      updated_torrents = [Torrent({id:1}), Torrent({id:2})];
-      torrent_helpers.removeOldTorrents(updated_torrents, old_torrents);
-      $('.torrent').length.should.eql(2);
+      updated_torrents = [Torrent({id:1}), Torrent({id:2})]
+      torrent_helpers.removeOldTorrents(updated_torrents, old_torrents)
+      $('.torrent').length.should.eql(2)
     end
     
     it 'should remove torrents that did not come in with the update'
-      updated_torrents = [Torrent({id:2})];
-      torrent_helpers.removeOldTorrents(updated_torrents, old_torrents);
-      $('.torrent').length.should.eql(1);
-      $('.torrent').attr('id').should.eql('2');
+      updated_torrents = [Torrent({id:2})]
+      torrent_helpers.removeOldTorrents(updated_torrents, old_torrents)
+      $('.torrent').length.should.eql(1)
+      $('.torrent').attr('id').should.eql('2')
     end
   end
   
@@ -28,34 +28,44 @@ describe 'TorrentHelpers'
     before_each
       template = fixture('../templates/torrents/show.mustache')
       // stub functions from other parts of the code
-      torrent_helpers.cache = function() { return template; };
-      torrent_helpers.updateInfo = function() {};
-      statusWord = function() {return 'seeding';};
+      torrent_helpers.cache = function() { return template; }
+      torrent_helpers.updateInfo = function() {}
+      statusWord = function() {return 'seeding';}
     end
     
     it 'should add a new torrent if it came in with the update and is not on the site yet'
       updated_torrents = [
         {'id':1, 'statusWord': statusWord},
         {'id':2, 'statusWord': statusWord},
-        {'id':3, 'statusWord': statusWord}
-      ];
-      torrent_helpers.updateTorrents(updated_torrents);
-      $('#3').get(0).should_not.be_undefined;
+        {'id':3, 'statusWord': statusWord},
+        {'id':4, 'statusWord': statusWord}
+      ]
+      torrent_helpers.updateTorrents(updated_torrents)
+      $('#4').get(0).should_not.be_undefined
     end
     
     it 'should remove an old torrent that did not come in with the update but is still on the site'
-      updated_torrents = [{'id':2, 'statusWord': statusWord}];
-      torrent_helpers.updateTorrents(updated_torrents);
-      $('#1').get(0).should.be_undefined;
+      updated_torrents = [{'id':2, 'statusWord': statusWord}]
+      torrent_helpers.updateTorrents(updated_torrents)
+      $('#1').get(0).should.be_undefined
     end
     
     it 'should update the torrents\' data'
       updated_torrents = [
         {'id':1, 'statusWord': statusWord, 'statusString': 'Seeding - DL: 0.0 KB/s, UL: 20.0 KB/s'},
         {'id':2, 'statusWord': statusWord}
-      ];
-      torrent_helpers.updateTorrents(updated_torrents);
+      ]
+      torrent_helpers.updateTorrents(updated_torrents)
       $('#1').find('.statusString').html().should.match(/20\.0 KB\/s/)
+    end
+    
+    it 'should remove the meta status if downloding started'
+      updated_torrents = [
+        Torrent({'id':3, 'statusWord': statusWord, 'metadataPercentComplete': 1})
+      ]
+      torrent_helpers.updateTorrents(updated_torrents)
+      $('#3').find('.progressDetails').html().should_not.match(/metadata/)
+      $('#3').find('.progressbar').find('.ui-widget-header-meta').get(0).should.be_undefined
     end
   end
 end
