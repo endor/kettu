01e13d129d2d1a37e1b1c5c0c89b04cc1c7febe9
endor <fp@notjusthosting.com>
2010-04-09 13:43:18 +0300

    extend stats view

--- js/views/statistics.js
+++ js/views/statistics.js
@@ -2,8 +2,11 @@ StatisticsView = function(statistics) {
   var view = {
     'number_of_torrents': statistics['torrentCount'],
     'uploaded': Math.formatBytes(statistics['current-stats']['uploadedBytes']),
+    'uploaded_total': Math.formatBytes(statistics['cumulative-stats']['uploadedBytes']),
     'downloaded': Math.formatBytes(statistics['current-stats']['downloadedBytes']),
-    'time_active': Math.formatSeconds(statistics['current-stats']['secondsActive'])
+    'downloaded_total': Math.formatBytes(statistics['cumulative-stats']['downloadedBytes']),
+    'time_active': Math.formatSeconds(statistics['current-stats']['secondsActive']),
+    'time_active_total': Math.formatSeconds(statistics['cumulative-stats']['secondsActive'])
   }
   
   return view;
