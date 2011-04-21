8e1efc84f4bdd202f96d912e0866845d31bb8e70
endor <fp@notjusthosting.com>
2011-04-21 11:50:46 +0300

    add "randomize port on launch" and "automatically map port" options to network settings

--- js/models/settings_validator.js
+++ js/models/settings_validator.js
@@ -3,7 +3,8 @@ var SettingsValidator = function() {
     'presence_of': [
       'peer-port', 'alt-speed-down','alt-speed-up', 'dht-enabled', 'download-dir', 
       'encryption', 'peer-port', 'pex-enabled', 'speed-limit-down', 'speed-limit-down-enabled',
-      'speed-limit-up', 'speed-limit-up-enabled', 'utp-enabled'
+      'speed-limit-up', 'speed-limit-up-enabled', 'utp-enabled', 'peer-port-random-on-start',
+      'port-forwarding-enabled'
     ],
     'numericality_of': [
       {'field': 'peer-port', 'max': 65535},
