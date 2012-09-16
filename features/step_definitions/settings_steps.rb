03073b0fd01207452a4a8424eb43c36b4c904873
endor <fp@notjusthosting.com>
2012-09-16 14:02:21 +0200

    move integration tests to different location

--- features/step_definitions/settings_steps.rb
+++
@@ -1,3 +0,0 @@
-Then /there should be a request to set "([^\"]+)" to "([^\"]+)"/ do |setting, value|
-  File.read(File.dirname(__FILE__) + '/../support/last_request.json').should include(%Q{"#{setting}":#{value}})
-end
